a476494963e6b0173f37928ab942d2808bfc3622
KiraLaird <107937638+KiraLaird@users.noreply.github.com>
2022-07-15 15:10:45 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,15 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         int credit = (int) service.calculate(1000000, 9.99, 12);
 
-        System.out.println(" Ежемесячный платеж:" + (credit) + " руб.");
-
         System.out.println("");
 
         credit = (int) service.calculate(1000000, 9.99, 24);
-
-        System.out.println(" Ежемесячный платеж:" + (credit) + " руб.");
 
         System.out.println("");
 
         credit = (int) service.calculate(1000000, 9.99, 36);
 
-        System.out.println(" Ежемесячный платеж:" + (credit) + " руб.");
-
     }
 }
+
 
