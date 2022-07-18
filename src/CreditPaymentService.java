6739b30c1eca82733911fb524d7bbc8407130734
KiraLaird <107937638+KiraLaird@users.noreply.github.com>
2022-07-18 18:37:41 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,16 +2,13 @@ public class CreditPaymentService {
 
     private double credit;
 
-    public int calculate(int a, double b, int c) {
-        int amount = a; //  сумма кредита
-        double monthRate = b / 12 / 100; //  процентная ставка в месяц, равная годовой ставке, поделённой на 12
-        double yearRate = b;
-        int tenor = c; //  количество платежей
+    public int calculate(int amount, double monthRate, int tenor) {
+        monthRate = monthRate / 12 / 100;
         double ratio = (monthRate * Math.pow((1 + monthRate), tenor)) / (Math.pow((1 + monthRate), tenor) - 1);
         int credit = (int) (amount * ratio);
 
         System.out.println(" Сумма кредита:" + (amount) + " руб.");
-        System.out.println(" Процентная ставка:" + (yearRate) + " %.");
+        System.out.println(" Процентная ставка:" + (monthRate) + " %.");
         System.out.println(" Количество месяцев:" + (tenor));
         System.out.println(" Ежемесячный платеж:" + (credit) + " руб.");
 
